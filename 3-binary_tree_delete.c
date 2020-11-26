9bd4a7a87abf5d1a9367e5160729a441e6e39ecd
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-11-25 21:38:04 -0500

    tasks

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,12 +1,20 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_delete - Inserts a node right-child
+ * @tree: Root node of the binary tree
+ * Return: nothing
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree)
+	if (!tree)
+		return;
+	if (tree->left)
 	{
 		binary_tree_delete(tree->left);
-		binary_tree_delete(tree->right);
-		free(tree);
-        return;
 	}
+	if (tree->right)
+	{
+		binary_tree_delete(tree->right);
+	}
+	free(tree);
 }
